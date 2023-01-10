2b344aba62dc25faa93cdee78bc80bd92a97f8f5
bipmaz <118344639+bipmaz@users.noreply.github.com>
2023-01-10 12:19:07 +0300

    Первая версия

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from module_test import*
+import module_test
+
+
 if __name__ == "__main__":
-    with open('./answer.txt', 'r', encoding='UTF-8') as key:
+    with open('answer.txt', 'r', encoding='UTF-8') as key:
         list_answer = key.readline()
     instruction = 'Чтобы ответить на вопрос, необходимо ввести номер ответа. \nУдачи\n'
     for i in instruction:
         print(i, end='')
-        time.sleep(0.05)
-    with open('./question.txt', 'r', encoding='UTF-8') as file:
+        module_test.time.sleep(0.05)
+    with open('question.txt', 'r', encoding='UTF-8') as file:
         list_question = file.readlines()
         for index, line1 in enumerate(list_question):
             list_with_question = line1.split(':')
@@ -15,10 +17,10 @@ if __name__ == "__main__":
             var2 = list_with_question[2]
             var3 = list_with_question[3]
             keys = list_answer[index]
-            Question(question, var1, var2, var3).print_question()
+            module_test.Question(question, var1, var2, var3).print_question()
             answer = input()
-            Answer(keys, answer).answer_of_test()
-        result(list_answer)
+            module_test.Answer(keys, answer).answer_of_test()
+        module_test.result(list_answer)
 
 
 
--- module_test.py
+++ module_test.py
@@ -1,50 +1,60 @@
 from random import choice
+from colorama import init
 import os
 import time
 import pyglet
+
+
 errors = ['...', 'Серьезно?', 'Гугл не работает?']
 result_question = ['Плюс в карму', 'Мое почтение, держи за ответы', 'Заслужил']
 j = 0
-class Question:
 
+
+class Question:
+    """The class returns a question"""
     def __init__(self, question, var1, var2, var3):
+        """Determination of initial parameters"""
         self.question = question
         self.var1 = var1
         self.var2 = var2
         self.var3 = var3
 
     def print_question(self):
+        """Output on display"""
         message = self.question + '\n' + self.var1 + '\n'+ self.var2 + '\n'+ self.var3
         for i in message:
             time.sleep(0.05)
-            print('\033[0m{}'.format(i), end='')
+            init()
+            print('\033[39m{}'.format(i), end='')
 
 class Answer:
-
+    """The class outputs a response depending on the value of the input"""
     def __init__(self, keys, answer):
-
+        """Determination of initial parameters"""
         self.keys = keys
         self.answer = answer
 
     def answer_of_test(self):
-
+        """Output a message depending on the response"""
         if self.answer == self.keys:
             global j
             j += 1
             variant = choice(result_question) + ' ' + str(j) + '\n'
             for symbol in variant:
                 time.sleep(0.05)
+                init()
                 print("\033[32m{}".format(symbol), end='')
 
         else:
             variant = choice(errors) + '\n'
             for symbol in variant:
                 time.sleep(0.5)
+                init()
                 print("\033[31m{}".format(symbol), end='')
 
 
 def result(list_answer):
-
+    """Output of the result depending on the number of points scored"""
     if j >= len(list_answer)*0.75:
          message = 'Молодец, держи бездну'
          for i in message:
